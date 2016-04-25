ffb1910d9ced579802385737d880a829e32e8a9d
Denis Mokhin <mokhin.denis@yandex.ru>
2016-04-25 09:51:38 +0300

    Just updated website

--- helper.php
+++ helper.php
@@ -3,11 +3,11 @@
 *
 * MyAnniversary tells you about anniversaries of a current day
 *
-* Copyright (C) 2012-2013 my-j.ru. All rights reserved. 
+* Copyright (C) 2012-2016 mokhin-tech.ru. All rights reserved. 
 *
 * Author is:
-* Denis E Mokhin < denis[at]my-j.ru >
-* http://my-j.ru
+* Denis E Mokhin < denis@mokhin-tech.ru >
+* http://mokhin-tech.ru
 *
 * @license GNU GPL, see http://www.gnu.org/licenses/gpl-2.0.html
 * 
